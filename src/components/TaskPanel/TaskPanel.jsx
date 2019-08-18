2cb749eeac0874854fdee58b2295a1a0fb9979c3
Lian <twobelian@gmail.com>
2019-08-19 07:39:01 +0800

    duration

--- src/components/TaskPanel/TaskPanel.jsx
+++ src/components/TaskPanel/TaskPanel.jsx
@@ -27,7 +27,21 @@ export default class TaskPanel extends Component {
         this.setState((prevState) => ({
             tasks: prevState.tasks.map((task) => {
                 if (task.key == key) {
-                    task[target.name] = target.value
+                    switch (target.name) {
+                        case 'duration':
+                            if (!!parseInt(target.value)) {
+                                let baseDate = new moment(task.startdate)
+                                baseDate.add(parseInt(target.value), "days")
+                                task.duedate = baseDate
+                            }
+                            break;
+                        case 'startdate':
+                        case 'duedate':
+                            task[target.name] = moment(target.value)
+                            break;
+                        default:
+                            task[target.name] = target.value
+                    }
                 }
                 return task
             })
@@ -41,32 +55,29 @@ export default class TaskPanel extends Component {
     }
 
 
-    renderRows() {
-        return this.state.tasks.map(item =>
-            <Row
-                item={item}
-                handleChange={this.handleChange}
-                handleDelete={this.handleDelete}
-                key={item.key}
-            />
+    renderTableBody() {
+        return (
+            <tbody>{
+                this.state.tasks.map(item =>
+                    <Row
+                        item={item}
+                        handleChange={this.handleChange}
+                        handleDelete={this.handleDelete}
+                        key={item.key}
+                    />
+                )
+            }
+            </tbody>
         )
     }
+
 
     render() {
         return (
             <div>
                 <table>
-                    <thead>
-                        <tr>
-                            <th>Task Name</th>
-                            <th>Start Date</th>
-                            <th>Due Date</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {this.renderRows()}
-                        <tr><td><button onClick={this.handleAddTask}>Add New Task</button></td></tr>
-                    </tbody>
+                    <TableHead />
+                    {this.renderTableBody()}
                     <tfoot>
                         <tr>
                             <td>Total Duration</td>
@@ -74,51 +85,81 @@ export default class TaskPanel extends Component {
                         </tr>
                     </tfoot>
                 </table>
+                <button onClick={this.handleAddTask}>Add New Task</button>
             </div>
         )
     }
 }
 
+const TableHead = () => (
+    <thead>
+        <tr>
+            <th>Task Name</th>
+            <th>Start Date</th>
+            <th>Due Date</th>
+            <th>Duration</th>
+        </tr>
+    </thead>
+)
+
 const newTask = () => (
     {
         key: uuid(),
         taskname: "new task",
-        startdate: moment().format("YYYY-MM-DD"),
-        duedate: moment().add(7, "days").format("YYYY-MM-DD"),
-        dureation: 0,
+        startdate: new moment(),
+        duedate: new moment().add(7, "days"),
         // tasks must be done before this task start
         prerequisites: []
     }
 )
 
+const InputeDateString = "YYYY-MM-DD"
+
 const Row = (props) => {
-    const item = props.item
-    const handleChange = props.handleChange
-    const handleDelete = props.handleDelete
+    const {
+        taskname,
+        key,
+        startdate,
+        duedate
+    } = props.item
+    const {
+        handleChange,
+        handleDelete
+    } = props
+
+
     return (
         <tr>
             <td>
                 <input
                     type="text"
                     name="taskname"
-                    defaultValue={item.taskname}
-                    onChange={(e) => handleChange(e, item.key)}>
+                    defaultValue={taskname}
+                    onChange={(e) => handleChange(e, key)}>
                 </input>
             </td>
             <td>
                 <input
                     type="date"
                     name="startdate"
-                    defaultValue={item.startdate}
-                    onChange={(e) => handleChange(e, item.key)}>
+                    value={startdate.format(InputeDateString)}
+                    onChange={(e) => handleChange(e, key)}>
                 </input>
             </td>
             <td>
                 <input
                     type="date"
                     name="duedate"
-                    defaultValue={item.duedate}
-                    onChange={(e) => handleChange(e, item.key)}>
+                    value={duedate.format(InputeDateString)}
+                    onChange={(e) => handleChange(e, key)}>
+                </input>
+            </td>
+            <td>
+                <input
+                    type="text"
+                    name="duration"
+                    value={duedate.diff(startdate, 'days')}
+                    onChange={(e) => handleChange(e, key)}>
                 </input>
             </td>
             <td>
@@ -126,7 +167,7 @@ const Row = (props) => {
                     type="button"
                     name="delete"
                     value="delete"
-                    onClick={() => handleDelete(item.key)}>
+                    onClick={() => handleDelete(key)}>
                 </input>
             </td>
         </tr>
